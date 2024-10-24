70e8dcc434004634eaa0763ad209db5fa63f6b4c
itgimpi <brejkersi@gmail.com>
2024-10-24 14:17:16 +0200

    Update par-nepar2.cpp

--- algo/rec/par-nepar2.cpp
+++ algo/rec/par-nepar2.cpp
@@ -3,14 +3,13 @@ using namespace std;
 typedef unsigned long long ull;
 // Функција пар-непар
 // https://petlja.org/sr-Latn-RS/biblioteka/r/Zbirka2/funkcija_par_nepar
-// 2 deljenja
+// samo sabiranje
 ull f( ull n ) {
     if (n == 1) return 1;
-    if ( n % 2 == 0 ) // nepar?
-        return f( n / 2 ) + f( n / 2 + 1 );
-        
+    if ( n & 1 ) // nepar?
+        return f( n >> 1 ) + f( n >> 1 + 1 );
     // par?
-    return f( n / 2 );
+    return f( n >> 1 );
 }
 
 int main() {
