9fe79ae80c6c4fd01312d31f77c542d0f8ba03cd
itgimpi <brejkersi@gmail.com>
2024-10-29 14:21:19 +0100

    rek

---
+++ algo/rec/izvodjenje/par-nepar-ite.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+typedef unsigned long long ull;
+
+ull f(ull  n) { 
+    ull a = 1, b = 0; // a je koef. uz prvi, na pocetku 1
+    while ( n > 1 ) {
+        ull n2 = n / 2; // bitski...
+        if ( n2 + n2 == n ) // n je par?
+            a += b; // azuriraj koef. prvog
+        else // nepar
+            b += a;
+        n = n2; }
+    return a + b;
+    }
+
+int main() {
+    ull n;  cin >> n;
+
+    cout << f(n);
+    return 0; }
