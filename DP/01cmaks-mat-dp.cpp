ba886e67795018ce230ec8d8352173e00faa8900
itgimpi <brejkersi@gmail.com>
2025-03-27 14:01:38 +0100

    dp

---
+++ DP/01cmaks-mat-dp.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Максимални збир на путу кроз матрицу
+// https://petlja.org/sr-Latn-RS/biblioteka/r/Zbirka2/max_zbir_puta_kroz_matricu
+// dp
+
+int najzbir(const vector<vector<int>>&mat) {
+    int n = mat.size();
+
+    vector<vector<int>>dp(n); // dp matrica sa resenjima
+    for ( int i = 0; i < n; i++ )
+        dp[i].resize(n);
+
+    dp[0][0] = mat[0][0]; // polje skroz gore levo
+
+    for ( int r = 1; r < n; r++) // nulta kolona
+        // gornje resenje + vredn. za to polje
+        dp[r][0] = dp[r-1][0] + mat[r][0];
+
+    for ( int k = 1; k < n; k++) // nulti red
+        // levo resenje + vredn. za to polje
+        dp[0][k] = dp[0][k-1] + mat[0][k];
+    
+    for ( int r = 1; r < n; r++ ) // za sve redove od reda 1...
+        for ( int k = 1; k < n; k++ ) // i sve kolona od kol. 1...
+            dp[r][k] = max( dp[r][k-1], dp[r-1][k] ) + mat[r][k];
+    return dp[n-1][n-1];
+}
+
+int main() {
+    int n; cin >> n; // dimenzija matrice
+    vector<vector<int>>mat(n); // 
+    for ( int i = 0; i < n; i++ ) {
+        mat[i].resize(n);
+        for ( int j = 0; j < n; j++ )
+            cin >> mat[i][j]; } 
+    cout << najzbir(mat); // top
+    }
